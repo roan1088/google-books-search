e304d03fbade5394045f4b43bc93ee1218d65ec0
roan1088 <rohan_188@yahoo.com>
2020-12-07 23:43:03 -0500

    Added Search and Saved pages

---
+++ client/src/pages/Saved/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Saved() {
+  return (
+    <p>Saved page</p>
+  );
+}
+
+export default Saved;
---
+++ client/src/pages/Search/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Search() {
+  return (
+    <p>Search page</p>
+  );
+}
+
+export default Search;
