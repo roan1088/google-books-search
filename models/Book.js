c59bffa5785bb9b750cbd9d3d5850a9cd483b645
roan1088 <rohan_188@yahoo.com>
2020-12-07 20:01:33 -0500

    Added Book model

---
+++ models/Book.js
@@ -0,0 +1,18 @@
+// Importing mongoose schema to create model
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Schema for the model
+const BookSchema = new Schema({
+  title: String,
+  authors: Array,
+  description: String,
+  image: String,
+  link: String
+});
+
+// Defining a model using the schema
+const Book = mongoose.model("Book", BookSchema);
+
+// Exporting the model
+module.exports = Book;
