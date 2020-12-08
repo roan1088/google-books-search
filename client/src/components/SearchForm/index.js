341e62cf10d8a55c741cf999c283250293c5af25
roan1088 <rohan_188@yahoo.com>
2020-12-08 14:39:15 -0500

    Updated SearchForm component

--- client/src/components/SearchForm/index.js
+++ client/src/components/SearchForm/index.js
@@ -1,9 +1,16 @@
 import React from "react";
+import "./style.css";
 
 function SearchForm() {
   return (
-    <form>
-      Search form
+    <form className="search-form">
+      <label htmlFor="book-title">Book Search</label>
+      <br />
+      <input type="text" name="book-title" placeholder="Book Title" />
+      <br />
+      <div className="button-area">
+        <button>Search</button>
+      </div>
     </form>
   );
 }
