0cfcb51e4eaeb5bb247d1a3e419ad3138223b08a
roan1088 <rohan_188@yahoo.com>
2020-12-08 13:32:04 -0500

    Updated the Navbar component

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./style.css";
 
 function Navbar() {
   return (
-    <nav>
-      Navbar
+    <nav className="nav">
+      <Link to="/" className="nav-link navBrand">Google Books</Link>
+      <Link to="/search" className="nav-link">Search</Link>
+      <Link to="/saved" className="nav-link">Saved</Link>
     </nav>
   );
 }
