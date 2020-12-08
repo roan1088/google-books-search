51a05b43eed8c158459528ea0cba348d75d00d75
roan1088 <rohan_188@yahoo.com>
2020-12-08 15:05:51 -0500

    Added a state to keep track of form input

--- client/src/components/SearchForm/index.js
+++ client/src/components/SearchForm/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./style.css";
 
-function SearchForm() {
+function SearchForm({ searchBook, setBook }) {
   return (
-    <form className="search-form">
+    <form className="search-form" onSubmit={searchBook}>
       <label htmlFor="book-title">Book Search</label>
       <br />
-      <input type="text" name="book-title" placeholder="Book Title" />
+      <input type="text" name="book-title" placeholder="Book Title" onChange={(e) => {setBook(e.target.value)}} />
       <br />
       <div className="button-area">
         <button>Search</button>
--- client/src/pages/Search/index.js
+++ client/src/pages/Search/index.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SearchForm from "../../components/SearchForm";
 import SearchResult from "../../components/SearchResult";
 
 function Search() {
+  useEffect(() => {
+    document.title = "Google Books Search";
+  }, []);
+
+  const [book, setBook] = useState("");
+
+  const searchBook = (e) => {
+    e.preventDefault();
+    console.log(book);
+  }
+
   return (
     <main>
-      <SearchForm />
+      <SearchForm searchBook={searchBook} setBook={setBook} />
       <SearchResult />
     </main>
   );
