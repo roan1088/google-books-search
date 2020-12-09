d21f5b8cdc5c4da55c59ee0822c8762b2db3240a
roan1088 <rohan_188@yahoo.com>
2020-12-08 19:10:28 -0500

    Added a method to post book to the db

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,5 +3,8 @@ import axios from "axios";
 export default {
   getGoogleBooks: function(title) {
     return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + title);
+  },
+  postBook: function(book) {
+    return axios.post("api/books", book);
   }
 };
