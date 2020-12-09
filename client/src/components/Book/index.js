d0f46cb5c634ca9bd125595790eebf5e3949d9f5
roan1088 <rohan_188@yahoo.com>
2020-12-08 19:10:57 -0500

    Added onclick on save button to save book to the db

--- client/src/components/Book/index.js
+++ client/src/components/Book/index.js
@@ -1,14 +1,22 @@
 import React from "react";
 import "./style.css";
+import API from "../../utils/API";
 
 function Book({ title, authors, image, description, link }) {
+  const saveBook = () => {
+    const book = {
+      title, authors, image, description, link
+    }
+    API.postBook(book).then(res => console.log(res));
+  }
+
   return (
     <div className="book-info">
       <div className="book-buttons">
         <button onClick={() => {
           window.open(link, "_blank");
         }}>View</button>
-        <button>Save</button>
+        <button onClick={saveBook}>Save</button>
       </div>
       <h5>{title}</h5>
       <h6>Written By: {authors.join(", ")}</h6>
