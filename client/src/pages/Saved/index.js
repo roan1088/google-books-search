439e376b55b16a2a4e834c4b7dd06590eea93e95
roan1088 <rohan_188@yahoo.com>
2020-12-08 20:28:30 -0500

    Updated Saved page to display all the saved books

--- client/src/pages/Saved/index.js
+++ client/src/pages/Saved/index.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Book from "../../components/Book";
+import API from "../../utils/API";
 
 function Saved() {
+  useEffect(() => {
+    document.title = "Saved Books";
+    API.getAllBooks().then(({ data }) => {
+      console.log(data);
+      setBooks(data);
+    })    
+  }, []);
+
+  const [books, setBooks] = useState([]);
+
   return (
-    <main>Saved page</main>
+    <main>
+      <p>Saved Books</p>
+      {books.map(book => <Book {...book} key={book._id} />)}
+    </main>
   );
 }
 
